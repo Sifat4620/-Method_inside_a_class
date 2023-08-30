6a67eaa94251e70357e968d95502d89834754a5b
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2023-08-30 21:16:47 +0600

    Add files via upload

---
+++  Method inside a class/src/Teacher.java
@@ -0,0 +1,7 @@
+public class Teacher {
+    public static void main(String[] args) {
+        teacher_info teacher1 =new teacher_info();
+        teacher1.name ="fahim Faisal sifat";
+        teacher1.displayInfo();
+    }
+}
---
+++  Method inside a class/src/teacher_info.java
@@ -0,0 +1,12 @@
+public class teacher_info {
+
+    String name ;
+
+    // return type dite hbe
+    void displayInfo(){
+
+        System.out.println("Name:"+ name);
+
+    }
+
+}
